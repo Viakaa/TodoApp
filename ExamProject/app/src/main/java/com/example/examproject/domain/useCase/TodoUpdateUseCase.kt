ecf8113373d5564808ec1c3b6e9e5c3723dd8835
Lincha <viket2006@gmail.com>
2025-05-15 21:06:14 +0300

    todo app

--- ExamProject/app/src/main/java/com/example/examproject/domain/useCase/TodoUpdateUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.examproject.domain.useCase
-
-import com.example.examproject.data.repository.TodoRepository
-import com.example.examproject.domain.Todo
-
-class TodoUpdateUseCase(private val repository: TodoRepository) {
-    suspend operator fun invoke(todo: Todo): Todo = repository.updateTodo(todo)
-}
--- ExamProject/app/src/main/java/com/example/examproject/domain/useCase/TodoUseCase.kt
+++
@@ -1,8 +0,0 @@
-    package com.example.examproject.domain.useCase
-
-    import com.example.examproject.data.repository.TodoRepository
-    import com.example.examproject.domain.Todo
-
-    class TodoUseCase(private val repository: TodoRepository) {
-        suspend operator fun invoke(): List<Todo> = repository.getTodos()
-    }
